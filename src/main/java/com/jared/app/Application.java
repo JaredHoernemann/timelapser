08dc40591088471bd3f9e93a8921212310ebe385
JaredHoernemann <jaredhoernemann@gmail.com>
2022-07-25 12:01:35 -0500

    supermicro-server

--- src/main/java/com/jared/app/Application.java
+++ src/main/java/com/jared/app/Application.java
@@ -2,11 +2,9 @@ package com.jared.app;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.jared.gson.ProjectDataGson;
-import com.jared.util.ImageUtils;
-import com.jared.util.FileUtils;
-import com.jared.util.Utils;
-import com.jared.camera.WebcamService;
+import com.jared.camera.WebcamCamera;
+import com.jared.utils.ImageUtils;
+import com.jared.utils.Utils;
 
 import java.io.File;
 
@@ -31,7 +29,7 @@ public class Application {
     private static void loop() {
         while (true) {
             try {
-                File file = WebcamService.takePicture("C:\\Users\\Jared\\IdeaProjects\\timelapser\\target\\webcam-captures\\");
+                File file = WebcamCamera.takePicture("C:\\Users\\Jared\\IdeaProjects\\timelapser\\target\\webcam-captures\\");
                 if (!ImageUtils.isLightOn(file)) {
                     System.out.println("Lights are off, deleting file: " + file.getName());
                     file.delete();
@@ -43,26 +41,5 @@ public class Application {
             }
             Utils.sleepForMinutes(TAKE_PIC_INTERVAL_MINS);
         }
-    }
-
-    private static void initialize(String projectName) {
-//        String dir = BASE_OUTPUT_DIR + projectName + "\\";
-//        FileUtils.ensureDirectoryExists(dir);
-//        String filePath = dir + METADATA_TXT;
-//
-//        if (!ProjectDataUtil.hasProjectData(dir)) {
-//            ProjectDataGson meta = new ProjectDataGson();
-//            meta.setProjectName(projectName);
-//            meta.setIntervalMinutes(TAKE_PIC_INTERVAL_MINS);
-//            meta.setStartTimeMillis(System.currentTimeMillis());
-//            meta.setProjectDirectory(dir);
-//            meta.setSproutDateMillis(AF_SPROUT_MILLIS);
-//
-//            String json = gson.toJson(meta);
-//            FileUtils.writeToFile(json, filePath);
-//        }
-//
-//        String json = FileUtils.readFileAsString(filePath);
-//        System.out.println(json);
     }
 }
